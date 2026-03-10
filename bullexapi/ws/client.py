4bbb2561effc1863f9938188bf1c0734509bc4cc
whsouza22 <wstrader@wstrader.onmicrosoft.com>
2026-03-10 08:07:38 -0400

    fix: bullexapi on_close missing wss parameter

--- bullexapi/ws/client.py
+++ bullexapi/ws/client.py
@@ -182,9 +182,10 @@ class WebsocketClient(object):
         logger.debug("Websocket client connected.")
         global_value.check_websocket_if_connect = 1
 
-    def on_close(self, close_status_code=None, close_msg=None):
+    def on_close(self, wss, close_status_code=None, close_msg=None):  # pylint: disable=unused-argument
         """Called when websocket connection is closed.
         
+        :param wss: Websocket client instance.
         :param close_status_code: Status code for close connection.
         :param close_msg: Message explaining why connection was closed.
         """
